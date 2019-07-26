35ba0516b689b4e9c8bc7705bc9fb793ce55b792
bsalyp <bsalyp@cisco.com>
2019-07-26 20:10:45 +0300

    Added watermark

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ FONT = 'fonts/arial.ttf'
 RESULT_IMAGE_PATH = 'img/result.jpg'
 
 
+def draw_watermark(image, watermark_text):
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype(FONT, 50)
+    draw.text((image.size[0] - font.getsize(watermark_text)[0], image.size[1] - font.getsize(watermark_text)[1]), watermark_text, (128,128,128), font=font)
+
+
 def split_to_lines(message, symbols_to_fit):
     new_lines = []
     current_word = ''
@@ -97,6 +103,9 @@ def handle_ok(message):
             draw.text((text_position_x, text_position_y), line, (0,0,0), font=font)
             text_position_y += font.getsize('A')[1]
 
+    # Watermark
+    draw_watermark(new_image, 'vk.com/ultimate_meme_bot')
+    
     # Resize an image
     wert = new_image.resize((int(whitespace_size[0] / 4), int(whitespace_size[1] / 4)), Image.ANTIALIAS)
     wert.resize((100, 100), Image.ANTIALIAS)
@@ -121,15 +130,6 @@ def handle_poker_face(message):
     new_image.paste(original_img, new_position)
     
     # Add text
-    # font = ImageFont.truetype(FONT, 140)
-    # text_position_x = int(whitespace_size[0] / 4)
-    # text_position_y = int(whitespace_size[0] / 7)
-    # text_position = (text_position_x, text_position_y)
-    # draw = ImageDraw.Draw(new_image)
-    # draw.text(text_position, message, (0,0,0), font=font)
-
-
-
     FONT_SIZES = [220, 200, 180, 150, 130, 100]
     TEXT_MARGIN_RIGHT = 0.05
     TEXT_MARGIN_TOP = 100
@@ -180,6 +180,9 @@ def handle_poker_face(message):
         for line in new_lines:
             draw.text((text_position_x, text_position_y), line, (0,0,0), font=font)
             text_position_y += font.getsize('A')[1]
+
+    # Watermark
+    draw_watermark(new_image, 'vk.com/ultimate_meme_bot')
 
     # Resize an image
     wert = new_image.resize((int(whitespace_size[0] / 4), int(whitespace_size[1] / 4)), Image.ANTIALIAS)
@@ -255,6 +258,9 @@ def handle_poker_face_2(message):
         for line in new_lines:
             draw.text((text_position_x, text_position_y), line, (0,0,0), font=font)
             text_position_y += font.getsize('A')[1]
+
+    # Watermark
+    draw_watermark(new_image, 'vk.com/ultimate_meme_bot')
 
     # Resize an image
     wert = new_image.resize((int(whitespace_size[0] / 4), int(whitespace_size[1] / 4)), Image.ANTIALIAS)
@@ -332,6 +338,9 @@ def handle_poker_face_3(message):
             draw.text((text_position_x, text_position_y), line, (0,0,0), font=font)
             text_position_y += font.getsize('A')[1]
 
+    # Watermark
+    draw_watermark(new_image, 'vk.com/ultimate_meme_bot')
+
     # Resize an image
     wert = new_image.resize((int(whitespace_size[0] / 4), int(whitespace_size[1] / 4)), Image.ANTIALIAS)
     wert.resize((100, 100), Image.ANTIALIAS)
@@ -356,15 +365,6 @@ def handle_me_only(message):
     new_image.paste(original_img, new_position)
     
     # Add text
-    # font = ImageFont.truetype(FONT, 80)
-    # text_position_x = int(whitespace_size[0] / 4)
-    # text_position_y = int(whitespace_size[1] / 7)
-    # text_position = (text_position_x, text_position_y)
-    # draw = ImageDraw.Draw(new_image)
-    # draw.text(text_position, message, (0,0,0), font=font)
-
-
-
     FONT_SIZES = [150, 120, 100, 80]
     TEXT_MARGIN_RIGHT = 0.05
     TEXT_MARGIN_TOP = 100
@@ -417,12 +417,9 @@ def handle_me_only(message):
             draw.text((text_position_x, text_position_y), line, (0,0,0), font=font)
             text_position_y += font.getsize('A')[1]
 
-
-
-
-
-
-
+    # Watermark
+    draw_watermark(new_image, 'vk.com/ultimate_meme_bot')
+    
     # Resize an image
     wert = new_image.resize((int(whitespace_size[0] / 4), int(whitespace_size[1] / 4)), Image.ANTIALIAS)
     wert.resize((100, 100), Image.ANTIALIAS)
