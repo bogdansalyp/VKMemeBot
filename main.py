2fad8f98b5dd521b0e19eff60c2f633ed19f05e6
bsalyp <bsalyp@cisco.com>
2019-07-26 14:16:22 +0300

    Moved logic to outer functions

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ import json
 from PIL import Image, ImageFont, ImageDraw
 
 
-def white_bg_square(img, message):
-    "return a white-background-color image having the img in exact center"
+def handle_ok_sign(message):
+    image_path = 'img/result.jpg'
+
+    img = Image.open('img/ok.jpg')
     size = (int(max(img.size)*1.3), int(max(img.size)))
     print(type(size[0]))
     layer = Image.new('RGB', size, (255,255,255))
@@ -22,19 +24,19 @@ def white_bg_square(img, message):
     size_int = tuple((int((size[0] - img.size[0])/4), int(size[1] - img.size[1])))
     layer.paste(img, size_int)
     wert = layer.resize((int(size[0]/4), int(size[1]/4)), Image.ANTIALIAS)
-    return wert
+    wert.resize((100, 100), Image.ANTIALIAS)
+    wert.save(image_path)
+    return image_path
 
 
-def write_msg(user_id, output):
-    img = Image.open('img/ok.jpg')
-    square_one = white_bg_square(img, output['text'])
-    square_one.resize((100, 100), Image.ANTIALIAS)
-    square_one.save('img/result.jpg')
+def write_msg(user_id, message, image_path):
 
+    # Upload the photo
     upload_url = vk.method('photos.getMessagesUploadServer', {'peer_id': 0})['upload_url']
     response = requests.post(upload_url, files = {'photo': open('img/result.jpg', 'rb')})
     json_data = json.loads(response.text)
 
+    # Send a message with photo
     result = vk.method('photos.saveMessagesPhoto', {'server': json_data['server'], 'photo': json_data['photo'], 'hash': json_data['hash']})
     vk.method('messages.send', {'user_id': user_id, 'message': '', 'attachment': 'photo{}_{}'.format(result[0]['owner_id'], result[0]['id']), 'random_id': random.randint(1, 9999)})
 
@@ -59,8 +61,15 @@ for event in longpoll.listen():
         if event.to_me:
 
             print('New message:')
-            print(f'For me by: {event.user_id}', end='')
+            print(f'For me by: {event.user_id}')
 
-            write_msg(event.user_id, handle_msg(event.text))
+            # Create a text answer
+            answer = handle_msg(event.text)
+
+            # Create an answer and an image
+            image_path = handle_ok_sign(event.text)
+
+            # Send a message back
+            write_msg(event.user_id, answer, image_path)
 
             print('Text: ', event.text)
